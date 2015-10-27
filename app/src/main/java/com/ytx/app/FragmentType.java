a65fa4f6acdae33b13f163cc4664635922280142
runcow <651641783@qq.com>
2015-10-27 11:33:34 +0800

    order confirm

--- app/src/main/java/com/ytx/app/FragmentType.java
+++ app/src/main/java/com/ytx/app/FragmentType.java
@@ -9,5 +9,7 @@ public class FragmentType {
     public final static int COUPONS_FRAGMENT = 1; // 优惠券
     public final static int PAY_FRAGMENT = 2; // 支付
     public final static int ORDER_CONFIRM_FRAGMENT = 3; // 填写订单
+    public final static int PAY_SUCCESS_FRAGMENT = 4; // 支付成功
+    public final static int PAY_FAIL_FRAGMENT = 5; // 支付失败
 
 }
